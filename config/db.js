11eeb635773e2303597a692261bca8f11b2144e7
Cade Nichols <c@codinghouse.co>
2016-08-06 11:16:18 -0700

    ready for heroku, with jawsdb!

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+const connection = mysql.createConnection(process.env.JAWSDB_URL || {
   host: 'localhost',   // url of database
   user: 'root',
   password: process.env.MYSQL_PASSWORD,
